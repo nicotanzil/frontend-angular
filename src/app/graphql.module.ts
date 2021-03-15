d211e18c1da6def86ee65b7fcbc6b84cc5067872
Nico Tanzil <63032763+nicotanzil@users.noreply.github.com>
2021-03-15 23:38:28 +0700

    Final push for hosting

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,8 +3,8 @@ import {APOLLO_OPTIONS} from 'apollo-angular';
 import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
 
-const uri = 'http://localhost:8080/query'; // <-- add the URL of the GraphQL server here
-// const uri = 'https://staem-go.herokuapp.com/query';
+// const uri = 'http://localhost:8080/query'; // <-- add the URL of the GraphQL server here
+const uri = 'https://staem-go.herokuapp.com/query';
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri, withCredentials: true}),
